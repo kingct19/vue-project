575017864fc45376bcc901e10a6e764570b49443
kingct19 <kingchandler19@gmail.com>
2024-04-21 19:42:58 -0500

    Header started

---
+++ src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <header class="header">
+        <nav class="navbar">
+       
+            <div class="logo">
+            <router-link to="/" class="logo-link">Your Name</router-link>
+            </div>
+            
+            <ul class="nav-menu">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link">Home</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/about" class="nav-link">About</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/projects" class="nav-link">Projects</router-link>
+                </li>
+
+                <li class="nav-item">
+                    <router-link to="/contact" class="nav-link">Contact</router-link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+header {
+    background-color: #333;
+    color: white;
+    padding: 1rem 2rem;
+}
+
+
+
+</style>
